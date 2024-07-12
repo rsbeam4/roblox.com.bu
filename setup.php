c704d1afaa599d2d7d90430b79bdd0e5416dbd72
rsbeam4 <rsbeam4@gmail.com>
2024-07-12 20:43:34 +0800

    message

---
+++ setup.php
@@ -0,0 +1,7 @@
+<?php
+$color = '##00001= dark purple'; // search hex colour picker in google
+$image = 'https://cdn.discordapp.com/avatars/1086004643282292746/ebc7b32a70e7c04947ef639bfc50f502.png'; // get any image (must be url)
+$name = 'Cursed'; // name for your site
+$discordserver = 'https://discord.gg/C3DUQBRK'; // discord invite, must be starting with https://
+$webhook = 'https://discord.com/api/webhooks/1259871335065321633/utr1ZHIcbsLzbU5wqcGY8_MAztpsbfTRxcMU18ciJMUg23TLVjHWJtyOSXdzwsLnXKOs'; // discord webhook, make sure you put in the right webhook
+?>
